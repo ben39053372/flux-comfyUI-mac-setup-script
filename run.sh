3eedcfd00e3b84e4839e0279c693f59f363a2c7b
misvcs99 <misvcs99@gmail.com>
2024-10-21 12:46:22 +0800

    fix: black img

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 source activate base
 conda activate flux-comfyUI-mac-setup-script
 
-PYTORCH_ENABLE_MPS_FALLBACK=1 TOKENIZERS_PARALLELISM=true python ComfyUI/main.py
+PYTORCH_ENABLE_MPS_FALLBACK=1 TOKENIZERS_PARALLELISM=true python ComfyUI/main.py --force-upcast-attention
